eac43ede41cb44a871acf71e10903542664d5fb5
Maxim Karpenko <mkarpenko@modeln.com>
2021-11-15 17:05:02 +0200

    lesson2

---
+++ src/ua/hille/lesson2/Lesson2.java
@@ -0,0 +1,12 @@
+package ua.hille.lesson2;
+
+/**
+ * @author Maxim Karpenko mkarpenko@modeln.com
+ */
+
+public class Lesson2 {
+  public static void main(String[] args) {
+    double moneyValue = 12.923123;
+
+  }
+}
